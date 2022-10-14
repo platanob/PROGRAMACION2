29689441cec4adefdd02a15920f6458bf91bc7a1
platanob <bcarrasco2022@alu.uct.cl>
2022-10-13 21:25:19 -0300

    era

--- programacion/programacion_2/dfgdf.py
+++
@@ -1,6 +0,0 @@
-j = [2,3,45]
-k = [2,3]
-print(j)
-for ol in k:
-    j.remove(ol)
-print(j)
--- programacion/programacion_2/proyecto_2.0.py
+++ programacion/programacion_2/proyecto_2.0.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import pygame as py
 
 
 @dataclass
@@ -18,7 +19,7 @@ class vehiculo:
         else :
             for conte in losconte:
                 if self.capacidad > 0 :
-                    if self.capacidad > 1:
+                    if self.capacidad >= 1:
                         self.l_contenedores.append(conte)
                         ol.append(conte)
                         if conte.tamano == "grande" :
@@ -49,7 +50,7 @@ class vehiculo:
         else:
             return False
 class camion(vehiculo):
-    nombre:str = "avion"
+    nombre:str = "camion"
     costo:int = 500000
     capacidad:int = 1
     l_contenedores = []
@@ -457,11 +458,9 @@ def veiculo_mete(contenedores):
             conetes = sacarcontenedores(2, c_p)
             conetes2 = sacarcontenedores(1, c_g)
             ve = camion()
-
             elimina = ve.meter_contenedores(conetes)
             for kaka in elimina:
                 c_p.remove(kaka)
-            
             elimina = ve.meter_contenedores(conetes2)
             for kaka in elimina:
                 c_g.remove(kaka)
@@ -502,6 +501,12 @@ def veiculo_mete(contenedores):
             for kaka in elimina:
                 c_g.remove(kaka)
             lis_veiculos.append(ve)
+        if c_g == None  and c_p == None:
+            tamaño = 0
+        elif c_g == None :
+            tamaño = len(c_p)/2
+        elif c_p == None:
+            tamaño = len(c_g)
         tamaño = len(c_g) + (len(c_p)/2)
 
     for pesoss in lis_veiculos:
@@ -528,16 +533,224 @@ for k in lista_productos:
 
 lista_con = meter_productos(lista_productos)
 
-
-
 lista_vehiculos = veiculo_mete(lista_con)
+
 for vei in lista_vehiculos:
     vei.imprimir()
-""" j = producto(2, 'papas', 'normal', 'solido', 22)
-k = producto(6, 'berengena', 'normal', 'solido', 5)
-kl = []
-kl.append(j)
-kl.append(k)
-ol = meter_productos(kl)
-for po in ol :
-    print(po.imprimir()) """
+
+
+
+def botones():
+    smallfont = py.font.SysFont('Corbel',30) 
+    pantalla.blit(smallfont.render("1.ver cantidad total de vehiculos ",True,blanco ), (20,40))
+    pantalla.blit(smallfont.render("2.ver cantidad total de cada tipo de vehiculo ",True,blanco ), (20,80))
+    pantalla.blit(smallfont.render("3.ver conteneidos de los vehiculos ",True,blanco ), (20,120))
+    pantalla.blit(smallfont.render("4.costo de transporte ",True,blanco ), (20,160))
+    pantalla.blit(smallfont.render("5.salir ",True,blanco ), (20,200))
+    pantalla.blit(smallfont.render("ESC.menu ",True,blanco ), (0,450))
+
+def can_vei(lisve):
+    smallfont = py.font.SysFont('Corbel',50) 
+    pantalla.blit(smallfont.render("vehiculos",True,blanco ), (160,120))
+    pantalla.blit(smallfont.render(str(len(lisve)),True,blanco ), (220,200))
+
+def contipovei(lisve):
+    global im_avion , im_barco ,im_camion ,im_tren
+    c = 0
+    a = 0
+    t = 0
+    b = 0
+    for el in lisve:
+        if el.nombre == 'camion':
+            c += 1
+        elif el.nombre == 'avion':
+            a += 1
+        elif el.nombre == 'tren':
+            t += 1
+        elif el.nombre == 'barco':
+            b += 1
+    pantalla.blit(im_avion , (250,0))
+    pantalla.blit(im_camion , (0,0))
+    pantalla.blit(im_tren , (250,250))
+    pantalla.blit(im_barco , (0,250))
+
+    smallfont = py.font.SysFont('Corbel',50) 
+    pantalla.blit(smallfont.render(str(c),True,rojo ), (100,100))
+    pantalla.blit(smallfont.render(str(a),True,rojo ), (350,100))
+    pantalla.blit(smallfont.render(str(t),True,rojo ), (350,350))
+    pantalla.blit(smallfont.render(str(b),True,rojo ), (100,350))
+
+def mostrar():
+    smallfont = py.font.SysFont('Corbel',30) 
+    pantalla.blit(smallfont.render('digite el numero del vehiculo',True,blanco ), (100,100))
+
+def mosdavei(num , lisve ):
+    try:
+        global o
+        numero = num -1
+        posy = 0
+        smallfont = py.font.SysFont('Corbel',15) 
+        smallfont2 = py.font.SysFont('Corbel',30) 
+        pantalla.fill(negro)
+        con_nor = 0
+        con_re = 0
+        con_in = 0
+        p_nor = 0
+        p_re = 0
+        p_in = 0
+        p_so = 0
+        p_li = 0
+        p_ga = 0
+        for con in lisve[numero].l_contenedores:
+            pantalla.blit(smallfont.render(str(con),True,rojo ), (0,posy))
+            posy += 20
+            if con.tipo_carga == 'normal':
+                con_nor += 1
+                p_nor += con.peso
+            elif con.tipo_carga == 'refrigerada':
+                con_re += 1
+                p_re += con.peso
+            elif con.tipo_carga == 'inflamable':
+                con_in += 1
+                p_in += con.peso
+            if con.masa == 'solida' :
+                p_so += con.peso
+            elif con.masa == 'liquido' :
+                p_li += con.peso
+            elif con.masa== 'gas':
+                p_ga += con.peso
+        py.display.update()
+        py.time.wait(3000)
+        pantalla.fill(negro)
+        pantalla.blit(smallfont2.render(('contenedores de tipo nomrla : '+str(con_nor)),True,rojo ), (0,100))
+        pantalla.blit(smallfont2.render(('contenedore de tipo refrigerado : '+str(con_re)),True,rojo ), (0,160))
+        pantalla.blit(smallfont2.render(('contenedores de tipo inflamable : '+str(con_in)),True,rojo ), (0,230))
+        py.display.update()
+        py.time.wait(3000)
+        pantalla.fill(negro)
+        pantalla.blit(smallfont2.render(('toneladas total de los productos :'+str(lisve[numero].peso)),True,rojo ), (0,100))
+        py.display.update()
+        py.time.wait(3000)
+        pantalla.fill(negro)
+        pantalla.blit(smallfont2.render(('toneladas de contenedores normal: '+str(p_nor)),True,rojo ), (0,100))
+        pantalla.blit(smallfont2.render(('toneladas de contenedores refrigerados: '+str(p_re)),True,rojo ), (0,160))
+        pantalla.blit(smallfont2.render(('toneladas de contenedores inflamable: '+str(p_in)),True,rojo ), (0,230))
+        py.display.update()
+        py.time.wait(3000)
+        pantalla.fill(negro)
+        pantalla.blit(smallfont2.render(('toneladas de masa solida: '+str(p_so)),True,rojo ), (0,100))
+        pantalla.blit(smallfont2.render(('toneladas de masa liquida: '+str(p_li)),True,rojo ), (0,160))
+        pantalla.blit(smallfont2.render(('toneladas de masa gas: '+str(p_ga)),True,rojo ), (0,230))
+        py.display.update()
+        py.time.wait(3000)
+        o = 3 
+    except : 
+        o = 3
+def ptrans(lisve):
+    pt = 0
+    p_c = 0
+    p_a = 0
+    p_t = 0
+    p_b = 0
+    for ko in lisve:
+        pt = ko.costo
+        if ko.nombre == 'camion':
+            p_c += ko.costo
+        elif ko.nombre == 'avion':
+            p_a += ko.costo
+        elif ko.nombre == 'tren':
+            p_t += ko.costo
+        elif ko.nombre == 'barco':
+            p_b += ko.costo
+    smallfont = py.font.SysFont('Corbel',30) 
+    pantalla.blit(smallfont.render(('costo de camiones : '+ str(p_c)),True,blanco ), (0,100))
+    pantalla.blit(smallfont.render(('costo de avion : '+ str(p_a)),True,blanco ), (0,150))
+    pantalla.blit(smallfont.render(('costo de tren : '+ str(p_t)),True,blanco ), (0,200))
+    pantalla.blit(smallfont.render(('costo de barco : '+ str(p_b)),True,blanco ), (0,250))
+    pantalla.blit(smallfont.render(('costo total : '+ str(pt)),True,blanco ), (0,300))
+
+py.init()
+resolucion = (500,500)
+pantalla = py.display.set_mode(resolucion)
+negro = (0,0,0)
+blanco = (255,255,255)
+amarrilo = (255,255,0)
+rojo = (255,0,0)
+numero = 0
+o = 0
+im_camion = py.transform.scale(py.image.load("camion.jpg"),(250,250))
+im_avion = py.transform.scale(py.image.load('avion.jpg'),(250,250))
+im_tren = py.transform.scale(py.image.load('tre.jpg'),(250,250))
+im_barco = py.transform.scale(py.image.load('barco.jpg'),(250,250))
+
+while True :
+    for ev in py.event.get():
+
+        if ev.type == py.QUIT:
+            py.quit()
+        if ev.type == py.KEYDOWN:
+            if ev.key == py.K_1:
+                if o == 0:
+                    o = 1
+                elif o == 3 :
+                    o= 6
+                    numero = 1
+            if ev.key == py.K_2 :
+                if o == 0:
+                    o = 2
+                elif o == 3 :
+                    o= 6
+                    numero = 2
+            if ev.key == py.K_3:
+                if o == 0 :
+                    o= 3
+                elif o == 3 :
+                    o= 6
+                    numero = 3
+            if ev.key == py.K_4:
+                if o == 0:
+                    o = 4
+                elif o == 3 :
+                    o= 6
+                    numero = 4
+            if ev.key == py.K_5:
+                if o == 0:
+                    py.quit()
+                if o == 3 :
+                    o= 6
+                    numero = 5
+            if ev.key == py.K_6:
+                if o == 3 :
+                    o= 6
+                    numero = 6
+            if ev.key == py.K_7:
+                if o == 3 :
+                    o= 6
+                    numero = 7
+            if ev.key == py.K_8:
+                if o == 3 :
+                    o= 6
+                    numero = 8
+            if ev.key == py.K_9:
+                if o == 3 :
+                    o= 6
+                    numero = 9
+            if ev.key == py.K_ESCAPE:
+                    o=0
+
+    pantalla.fill(negro)
+    if o == 0 :
+        botones()
+    elif o == 1:
+        can_vei(lista_vehiculos)
+    elif o == 2:
+        contipovei(lista_vehiculos)
+    elif o == 3 :
+        mostrar()
+    elif o == 4:
+        ptrans(lista_vehiculos)
+    elif o == 6 :
+        mosdavei(numero, lista_vehiculos)
+    py.display.update()
+
+
